ca642ad0b8898c927d25ba1887896d82841cd833
Joaquin Gil Chanza <jogich@floridauniversitaria.es>
2016-10-20 10:18:35 +0200

    ximo

---
+++ multiplicar2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tabla del 2</title>
+  </head>
+  <body>
+    <?php
+      //Tabla del 2
+      $numero=0;
+      echo "<h2>Tabla del 2</h2>";
+      while ($numero<=10) {
+
+        $resultado=$numero*2;
+        echo "2x$numero=$resultado <br>";
+
+      $numero++;
+      }
+    ?>
+  </body>
+</html>
